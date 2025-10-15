158039b0f91d41448f2266997a9bdeec4058e868
Nitish-Gupta-18 <guptanitish2102@gmail.com>
2025-10-15 10:15:55 +0530

    Add files via upload

---
+++ Exp7Q1Main.java
@@ -0,0 +1,9 @@
+package exercises.java;
+
+public class Exp7Q1Main {
+    public static void main(String[] args) {
+        System.out.println("Nitish Gupta, 24csu330");
+        Exp7Q1Demo obj = new Exp7Q1Demo();
+        obj.display();
+    }
+}
